97d505fac649b977e77c7a97a785bb10a8a070ff
Ainsley Pluamdore <a.plumadore@setonhill.edu>
2020-05-02 20:42:29 -0400

    Ready

---
+++ Plumadore Homework #6/TBViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TBViewController.swift
+//  Plumadore Homework #6
+//
+//  Created by Ainsley Plumadore on 5/2/20.
+//  Copyright © 2020 Ainsley Plumadore. All rights reserved.
+//
+
+import UIKit
+
+class TBViewController: UITabBarController {
+
+    var yesVote: Int = 0
+    var noVote: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
